a4020eda283808eff539e3e7545ab2f675ed9d31
jonathan-dwight <jd.buendia0711@gmail.com>
2020-10-25 16:39:10 -0700

    Add Validators for login/register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require('bcryptjs')
-
+const keys = require("../../config/keys")
+const jwt = require('jsonwebtoken')
+const passport = require("passport")
+const validateRegisterInput = require("../../validation/register")
+const validateLoginInput = require("../../validation/login")
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the user route" });
@@ -10,6 +14,14 @@ router.get("/test", (req, res) => {
 
 
 router.post("/register", (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
@@ -33,9 +45,16 @@ router.post("/register", (req, res) => {
                 })
             }
         })
-    })
+})
 
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -48,7 +67,23 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch) {
-                        res.json({ msg: "success" })
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                })
+                            }
+                        )
+
                     } else {
                         return res.status(400).json({ passowrd: "Incorrect Password" })
                     }
