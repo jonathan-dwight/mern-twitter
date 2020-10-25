7feee9d3eeddaafa83cf2d2545b7deb9587c91e7
jonathan-dwight <jd.buendia0711@gmail.com>
2020-10-25 14:30:41 -0700

    Add Users Model and Routes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 
-//mongodb + srv://admin:<password>@mern.hd4uz.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
